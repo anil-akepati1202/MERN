75bcc5a8ccc0abbb1e360b579e2590b7334b9ef3
anil-akepati1202 <anil.akepati1202@gmail.com>
2020-07-14 18:52:24 +0100

    users model msg changes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -63,7 +63,7 @@ router.post(
         { expiresIn: 360000 },
         (err, token) => {
           if (err) throw err;
-          res.json({ token });
+          res.json({ token, id: user.id, name: user.name, email: user.email });
         }
       );
     } catch (err) {
